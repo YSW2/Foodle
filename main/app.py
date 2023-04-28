edec51b84f1a201af7a99ab567955205cb334497
YSW2 <jrgyber@gmail.com>
2023-04-28 14:24:06 +0900

    init commit

--- main/app.py
+++ main/app.py
@@ -1,19 +1,22 @@
 from flask import Flask, render_template, request, redirect, session
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 import re
+import secrets
+import hashlib
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'  # 데이터베이스 설정
-app.secret_key = 'secret-key'  # 세션 암호화를 위한 시크릿 키
+app.secret_key = secrets.token_hex(16)  # 세션 암호화를 위한 시크릿 키
 db = SQLAlchemy(app)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(30), nullable=False)
     username = db.Column(db.String(50), unique=True, nullable=False)
-    password = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(64), nullable=False)
     email = db.Column(db.String(50), nullable=True)
-    birth = db.Column(db.Integer, nullable=False)
+    birth = db.Column(db.Date, nullable=False)
 
 
 @app.route('/')
@@ -28,7 +31,7 @@ def register():
         username = request.form['username']  # POST 요청으로부터 username 파라미터 추출
         password = request.form['password']  # POST 요청으로부터 password 파라미터 추출
         email = request.form['email']
-        birth = request.form['birth']
+        birth = str(request.form['birth'])
         
         if not checkPwLen(password):
             return render_template('register.html', error='비밀번호는 4자리 이상 설정 가능합니다.')
@@ -38,14 +41,22 @@ def register():
         
         if not checkEmailForm(email):
             return render_template('register.html', error='이메일 형식이 올바르지 않습니다.')
+        
+        if not checkBirthForm(birth):
+            return render_template('register.html', error='생년월일을 올바르게 입력해주세요.')
 
         user = User(
-            name=name, username=username, password=password, email=email, birth=birth
-            )  # User 모델 객체 생성
+            name=name, username=username, password=hash_password(password), email=email, birth=datetime(int(birth[:4]), int(birth[4:6]), int(birth[6:8]))
+            )  # User 모델 객체 생성    
         db.session.add(user)  # 데이터베이스에 추가
         db.session.commit()  # 변경사항 저장
         return redirect('/login')  # 로그인 페이지로 리다이렉트
     return render_template('register.html')  # GET 요청일 경우 register.html 렌더링
+
+def hash_password(password):
+    hash = hashlib.sha256()
+    hash.update(password.encode('utf-8'))
+    return hash.hexdigest()
 
 def checkPwLen(password):
     if len(password) < 4:
@@ -66,6 +77,20 @@ def checkEmailForm(email):
         return False
     else:
         return True
+
+def checkBirthForm(birth):
+    birth_regex = r'^\d{8}$'
+    if not re.match(birth_regex, birth):
+        return False
+    # 생년월일이 유효한지 검사
+    try:
+        year = int(birth[:4])
+        month = int(birth[4:6])
+        day = int(birth[6:8])
+        datetime(year, month, day)
+        return True
+    except ValueError:
+        return False
     
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -74,7 +99,7 @@ def login():
         password = request.form['password']  # POST 요청으로부터 password 파라미터 추출
         # username과 password가 일치하는 User 객체 추출
         user = User.query.filter_by(
-            username=username, password=password).first()
+            username=username, password=hash_password(password)).first()
         if user is not None:
             session['user_id'] = user.id  # 세션에 user_id 추가
             return redirect('/dashboard')  # 대시보드 페이지로 리다이렉트
