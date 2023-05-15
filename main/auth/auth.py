d57f15713ecfe7b13fb08688c1c5251e55e80b57
YSW2 <jrgyber@gmail.com>
2023-05-15 15:36:40 +0900

    id_check init

--- main/auth/auth.py
+++ main/auth/auth.py
@@ -1,4 +1,4 @@
-from flask import render_template, redirect, request, session, url_for
+from flask import render_template, redirect, request, session, url_for, jsonify
 from auth.models import User
 from datetime import datetime
 from app import db
@@ -6,50 +6,66 @@ from auth import auth
 import hashlib
 import re
 
-username = ''
 
 @auth.context_processor
 def today_date():
     return dict(datetime=datetime)
 
-@auth.context_processor
-def avail_id():
-    return dict(avail_id=username)
+
+@auth.route('/check_id', methods=['POST'])
+def check_id():
+    username = request.get_json()['username']  # POST 요청으로부터 username 파라미터 추출
+    if len(username) == 0:
+        result = {'message': '아이디를 입력해주세요.'}
+        return jsonify(result)
+
+    available_id = checkDupId(username)
+    if not available_id:
+        result = {'message': '중복된 아이디입니다.'}
+    else:
+        result = {'message': '사용가능한 아이디입니다.'}
+
+    return jsonify(result)
+ 
+@auth.route('/check_form', methods=['POST'])
+def check_form():
+    name = request.get_json()['name']
+    username = request.get_json()['username']
+    password = request.get_json()['password']
+    email = request.get_json()['email']
+
+    if len(name) == 0 or len(username) == 0 or len(password) == 0 or len(email) == 0:
+        result = {'error': '정확한 정보를 입력해주세요.'}
+        return jsonify(result), 400
+
+    if not checkPwLen(password):
+        result = {'error': '비밀번호는 4자리 이상 설정 가능합니다.'}
+        return jsonify(result), 400
+
+    if not checkEmailForm(email):
+        result = {'error': '이메일 형식이 올바르지 않습니다.'}
+        return jsonify(result), 400
+    
+    result = {'success': '양식이 정상적으로 작성되었습니다.'}
+    return jsonify(result), 200
+
 
 @auth.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
-        name = request.form['name']
-        username = request.form['username']  # POST 요청으로부터 username 파라미터 추출
-        password = request.form['password']  # POST 요청으로부터 password 파라미터 추출
-        email = request.form['email']
-        birth = datetime.strptime(request.form['birth'], '%Y-%m-%d')
+        name = request.get_json()['name']
+        username = request.get_json()['username']
+        password = request.get_json()['password']
+        email = request.get_json()['email']
+        birth = datetime.strptime(request.get_json()['birth'], '%Y-%m-%d')
 
-        if "id_check_form" in request.form:
-            if len(username) == 0:
-                return render_template('register.html', error='ID를 입력해주세요.')
-            available_id = checkDupId(username)
-            if not available_id:
-                return render_template('register.html', error='이미 사용중인 ID 입니다.')
-            else:
-                return render_template('register.html', avail='사용 가능한 ID입니다.')
-        
-        elif "register_btn" in request.form:
-            if len(name) == 0 or len(username) == 0 or len(password) == 0 or len(email) == 0:
-                return render_template('register.html', error='정확한 정보를 입력해주세요.')
-            
-            if not checkPwLen(password):
-                return render_template('register.html', error='비밀번호는 4자리 이상 설정 가능합니다.')
-
-            if not checkEmailForm(email):
-                return render_template('register.html', error='이메일 형식이 올바르지 않습니다.')
-            
-            user = User(
-                name=name, username=username, password=hash_password(password), email=email, birth=birth
-            )  # User 모델 객체 생성
-            db.session.add(user)  # 데이터베이스에 추가
-            db.session.commit()  # 변경사항 저장
-            return redirect(url_for('auth.login'))  # 로그인 페이지로 리다이렉트
+        user = User(
+            name=name, username=username, password=hash_password(password), email=email, birth=birth
+        )  # User 모델 객체 생성
+        db.session.add(user)  # 데이터베이스에 추가
+        db.session.commit()  # 변경사항 저장
+        result = {'success': '양식이 정상적으로 작성되었습니다.'}
+        return jsonify(result), 200
         
     elif request.method == 'GET':
         return render_template('register.html', datetime=datetime)  # GET 요청일 경우 register.html 렌더링
