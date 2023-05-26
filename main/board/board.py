7d40da1f83af2b449bdad93248458a3ec48489bb
YSW2 <jrgyber@gmail.com>
2023-05-26 13:50:42 +0900

    post delete init

--- main/board/board.py
+++ main/board/board.py
@@ -1,4 +1,4 @@
-from flask import render_template, redirect, session, url_for, request, jsonify
+from flask import render_template, redirect, session, url_for, request, jsonify, abort
 from models import Post
 from app import db
 from board import board
@@ -14,7 +14,6 @@ def list_post():
     per_page = request.args.get('per_page',type=int,default=10)
     option = request.args.get('option',type=str,default='T')
     keyword = request.args.get('keyword',type=str,default='')
-    print(request.args)
     posts = Post.query.order_by(Post.created_at.desc())  # 최신순으로 게시글 정렬
     
         # 제목으로 검색
@@ -37,14 +36,14 @@ def list_post():
 def write_post():
     if 'user_id' not in session:
         return redirect(url_for('auth.login'))
-        
+    
     if request.method == 'POST':  # POST 요청일 경우
         title = request.form['title']  # 제목 가져오기
         content = request.form['content']  # 내용 가져오기
         author = session['username']  # 작성자 정보 가져오기
         now = datetime.now()  # 현재 시간 가져오기
         new_post = Post(title=title, content=content, author=author,
-                        created_at=now, view=0, like=0)  # 새로운 게시물 생성
+                        created_at=now, view=0, like=0,user_id=session['user_id'])  # 새로운 게시물 생성
         post_db_session = db.session(bind='post')  # 게시물 DB 세션 생성
         post_db_session.add(new_post)  # 새로운 게시물 추가
         post_db_session.commit()  # DB에 반영
@@ -58,12 +57,11 @@ def show_post(post_id):
     per_page = request.args.get('per_page',type=int,default=10)
     option = request.args.get('option',type=str,default='T')
     keyword = request.args.get('keyword',type=str,default='')
-    print(request.args)
     
     post = Post.query.get_or_404(post_id)
     post.view += 1
     db.session.commit()
-    return render_template('post.html', post=post, page=page, per_page=per_page, option=option, keyword=keyword)
+    return render_template('post.html', post=post,page=page,per_page=per_page,option=option,keyword=keyword)
 
 
 @board.route('/<int:post_id>', methods=['POST'])
@@ -75,16 +73,58 @@ def Like_post(post_id):
     return jsonify(response)
 
 
+@board.route('/delete/<int:post_id>', methods=['POST'])
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    page = request.args.get('page',type=int,default=1)
+    per_page = request.args.get('per_page',type=int,default=10)
+    option = request.args.get('option',type=str,default='T')
+    keyword = request.args.get('keyword',type=str,default='')
+    
+    # 게시물 작성자와 로그인한 사람이 동일하지 체크
+    if('user_id' not in session or post.user_id != session['user_id']):
+        abort(403)  # Forbidden error
+
+    db.session.delete(post)
+    db.session.commit()
+
+    return redirect(url_for('board.list_post',page=page,per_page=per_page,option=option,keyword=keyword))
+
+
+@board.route('/modify/<int:post_id>', methods=['POST'])
+def modify_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if('user_id' not in session or post.user_id != session['user_id']):
+        abort(403)  # Forbidden error
+    title = request.form.get('title')
+    content = request.form.get('content')
+
+    post.title = title
+    post.content = content
+    post.created_at = datetime.now()
+    db.session.commit()
+
+    return redirect(url_for('board.list_post'))
+    
+
 # paging 테스트를 위해 , 게시물 100개 작성
 @board.route('/hiddencreate')
 def create_posts():
     for i in range(1, 101):
         title = f"게시물 제목 {i}"
         content = f"게시물 내용 {random.randint(1, 100)}"
-        author = f"작성자 {i}"
+        author = session['username']
         new_post = Post(title=title, content=content, author=author,
-                        created_at=datetime.now(), view=0, like=0)  # 새로운 게시물 생성
+                        created_at=datetime.now(), view=0, like=0,user_id=session['user_id'])  # 새로운 게시물 생성
         db.session.add(new_post)  # 새로운 게시물 추가
     db.session.commit()
     print("게시물 생성이 완료되었습니다.")
+    return redirect(url_for('board.list_post'))
+
+
+# 게시물 모두 삭제
+@board.route('/hiddendelete')
+def delete_all_posts():
+    db.session.query(Post).delete()
+    db.session.commit()
     return redirect(url_for('board.list_post'))
